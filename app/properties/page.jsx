af9419291e812b522c593a21beff665d745bfcf7
jkClyde <aquin.randall@gmail.com>
2024-12-04 07:00:36 +0800

    fixed properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Under Construction...</div>
+    <section className='flex justify-center items-center py-[50px] md:py-[150px] h-[60vh]'>
+      <h1 className='text-[30px] md:text-[50px]'>Under Construction...</h1>
+    </section>
   )
 }
 
